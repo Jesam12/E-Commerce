93aa759e5cdf96e51011eb1e29d49aee184902a9
jesam12 <jmetro181@gmail.com>
2024-08-16 00:41:00 +0100

    Commit to repo

--- src/components/NavItems.jsx
+++ src/components/NavItems.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
+import logo from "../assets/images/logo/logo.png"
 
 const NavItems = () => {
     const [menuToggle, setMenuToggle] = useState(false);
@@ -16,14 +17,46 @@ const NavItems = () => {
         }
     })
   return (
-    <header>
+    <header className={`header-section style-4 ${headerFixed ? "header-fixed fadeInup" : ""}`}>
         {/* header top start */}
-        <div>
+        {/* Making the screen responsive at medium level */}
+        <div className={`header-top d-md-none ${socialToggle ? "open" : ""}`}>
             <div className='container'>
                 <div className='header-top-area'>
                     <Link to="/signup" className='lab-btn me-3'><span>Create Acccount</span></Link>
                     <Link to="/login">Log in</Link>
                 </div>
+            </div>
+        </div>
+
+        {/*Header bottom */}
+         <div className='header-bottom'>
+            <div className='container'>
+                <div className='header-wrapper'>
+                    {/* logo */}
+                    <div className='logo-search-active'>
+                        <div className='logo'>
+                            <Link to={"/"}>
+                                <img src={logo} alt=""/>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>  
+
+        {/* Menu area */}
+        <div className='menu-area'>
+            <div className='menu'>
+                <ul className={`lab-ul ${menuToggle ? "active" : ""}`}>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/shop">Shop</Link></li>
+                    <li><Link to="/blog">Blog</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
+                </ul>
+
+                <Link to="sign-up" className='lab-btn me-3 d-none d-md-block'>Create Account</Link>
             </div>
         </div>
     </header>
